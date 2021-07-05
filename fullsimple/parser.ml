f5ae40a294799a4dbee1b0cfac556bfb551f820f
minerva1129 <mail@mnhepba.me>
2021-07-05 15:13:57 +0900

    [fullsimple] make parser Applicative

--- fullsimple/parser.ml
+++ fullsimple/parser.ml
@@ -11,6 +11,7 @@ let token_rparen = token ")";;
 let token_identifier = (lexeme (lower <~> many (alpha_num <|> exactly '_'))) => implode;;
 
 let braces x = between token_lparen token_rparen x;;
+let (<*>) xf x = xf >>= fun f -> x >>= f % return;;
 let (let*) = (>>=);;
 
 let parse_Unit = token_Unit >> return Syntax.TyUnit;;
@@ -21,17 +22,15 @@ and ty input = chainr1 atomic_ty parse_arrow input;;
 
 let parse_identifier = token_identifier => fun r -> Syntax.TmVar r;;
 let parse_unit = token_unit >> return Syntax.TmUnit;;
-let parse_application = space >> return (fun l r -> Syntax.TmApp(l, r));;
-
-let rec parse_lambda input = (
+let parse_lambda =
   let* _ = token_backslash in
   let* x = token_identifier in
   let* _ = token_colon in
   let* tyT = ty in
   let* _ = token_dot in
-  let* t = term in
-  return (Syntax.TmAbs(x, tyT, t))
-) input
-and atomic_term input = (parse_unit <|> parse_identifier <|> braces term) input
+  return (fun t -> Syntax.TmAbs(x, tyT, t));;
+let parse_application = space >> return (fun l r -> Syntax.TmApp(l, r));;
+
+let rec atomic_term input = (parse_unit <|> parse_identifier <|> braces term) input
 and application_term input = chainl1 atomic_term parse_application input
-and term input = (application_term <|> parse_lambda) input
+and term input = (application_term <|> (parse_lambda <*> term)) input
